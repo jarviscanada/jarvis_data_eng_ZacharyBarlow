f8f0c1d2f965381c0bc6d33356f01480adeea9a1
Zach Barlow <ztbarlow66@gmail.com>
2021-03-02 16:58:31 +0000

    fixed some problems with ddl and commiting sql queries to run against and use for the cron

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS PUBLIC.host_info
     (
         id SERIAL           NOT NULL,
-        hostname            VARCHAR NOT NULL,
+        hostname            VARCHAR(100) NOT NULL,
         cpu_number          INTEGER NOT NULL,
         cpu_architechture   VARCHAR(10) NOT NULL,
         cpu_model           VARCHAR(50) NOT NULL,
@@ -19,10 +19,10 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_info
 CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
     (
         "timestamp"     TIMESTAMP NOT NULL,
-        host_id SERIAL  NOT NULL,
+        host_id         SERIAL  NOT NULL,
         memory_free     INTEGER NOT NULL,
         cpu_idle        REAL NOT NULL,
-        cpu_kernal      REAL NOT NULL,
+        cpu_kernel      REAL NOT NULL,
         disk_io         INTEGER NOT NULL,
         disk_available  INTEGER NOT NULL,
 
